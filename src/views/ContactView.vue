80a880e32a7dd1c4d7e96d3152b6d0612b7784a1
NaturalTwitch <awesomejunior2@gmail.com>
2025-11-01 15:47:13 -0400

    Fixing localization error

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -191,7 +191,7 @@ async function submitForm(e) {
     background: linear-gradient(90deg, var(--neon), var(--neon-2));
     -webkit-background-clip: text;
     background-clip: text;
-    -webkit-text-fill-color: transparent;
+    /* -webkit-text-fill-color: transparent; */
     font-weight: 700;
 }
 
