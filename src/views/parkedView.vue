cc7e5ecdd44da409e2fda311e4ea4b86386a07c6
NaturalTwitch <awesomejunior2@gmail.com>
2026-07-14 16:44:56 -0400

    rewriting the website

--- src/views/parkedView.vue
+++ src/views/parkedView.vue
@@ -17,7 +17,7 @@
 </template>
 
 <style scoped>
-.parked-page {
+/* .parked-page {
   min-height: 100vh;
   display: flex;
   justify-content: center;
@@ -25,7 +25,7 @@
   background: linear-gradient(135deg, #0f172a, #1e293b);
   font-family: Inter, Arial, sans-serif;
   color: white;
-}
+} */
 
 .card {
   text-align: center;
